78a79ffd7b95d08a1b488eee068f5174599bd768
xeth <adamhung12@gmail.com>
2022-03-28 12:48:07 +0800

    update go version & add Split Excel Function

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/xh-dev-go/xhUtils
 
-go 1.16
+go 1.18
 
 require (
 	github.com/google/uuid v1.3.0
--- stringUtils/stringUtils.go
+++ stringUtils/stringUtils.go
@@ -1,10 +1,37 @@
 package stringUtils
 
+import "strings"
+
 func IndexOfString(arr []string, search string) int {
-	for i,v := range arr {
-		if v==search {
+	for i, v := range arr {
+		if v == search {
 			return i
 		}
 	}
-	return -1;
+	return -1
+}
+
+func SplitCpExcel(msg string, trimSpace bool) [][]string {
+	var lines [][]string
+	tempSplit := strings.Split(msg, "\n")
+	for index, _ := range tempSplit {
+		if strings.HasSuffix(tempSplit[index], "\r") {
+			tempSplit[index] = strings.TrimRight(tempSplit[index], "\r")
+		}
+		line := tempSplit[index]
+
+		if index == len(tempSplit)-1 && tempSplit[index] == "" {
+		} else {
+			if trimSpace {
+				var tempLine []string
+				for _, word := range strings.Split(line, "\t") {
+					tempLine = append(tempLine, strings.Trim(word, " "))
+				}
+				lines = append(lines, tempLine)
+			} else {
+				lines = append(lines, strings.Split(line, "\t"))
+			}
+		}
+	}
+	return lines
 }
