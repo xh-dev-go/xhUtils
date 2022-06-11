cbf202d0c4c7b139a3292b7dea8039bb749d5603
xethhung <xeth@xethh.dev>
2022-06-12 03:52:17 +0800

    add StringConcat, FormatNum and RemoveFirst

--- stringUtils/stringUtils.go
+++ stringUtils/stringUtils.go
@@ -1,6 +1,29 @@
 package stringUtils
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+// RemoveFirst
+/*
+Remove the prefix characters and first separator
+
+s -> ABCDEF
+separator -> CD
+-----
+output:
+EF
+*/
+func RemoveFirst(s string, separator string) (string, bool) {
+	index := strings.Index(s, separator)
+	if index == -1 {
+		return s, false
+	} else {
+		return s[index+len(separator):], true
+	}
+}
 
 func IndexOfString(arr []string, search string) int {
 	for i, v := range arr {
@@ -11,7 +34,20 @@ func IndexOfString(arr []string, search string) int {
 	return -1
 }
 
-func SplitCpExcel(msg string, trimSpace bool) [][]string {
+// SplitCpExcel
+/*
+ split the {msg} by row with "\n" and split each row by "\n"
+
+ input:
+ "a\tb\tc\nd\te\tf"
+
+ output:
+ [
+ [a,b,c]
+ [d,e,f]
+ ]
+*/
+func SplitCpExcel(msg string, trimSpace bool) (line [][]string) {
 	var lines [][]string
 	tempSplit := strings.Split(msg, "\n")
 	for index, _ := range tempSplit {
@@ -33,5 +69,24 @@ func SplitCpExcel(msg string, trimSpace bool) [][]string {
 			}
 		}
 	}
-	return lines
+	return
+}
+
+// StringToBufIoReader
+/*
+Convert a string into *bufio.Reader
+*/
+func StringToBufIoReader(str string) *bufio.Reader {
+	return bufio.NewReader(strings.NewReader(str))
+}
+
+func FormatNum(index int, format string) string {
+	return fmt.Sprintf(format, index)
+}
+func StringConcat(index string, separator string, msg string, withNewLine bool) string {
+	if withNewLine {
+		return fmt.Sprintf("%s%s%s%s", index, separator, msg, "\n")
+	} else {
+		return fmt.Sprintf("%s%s%s", index, separator, msg)
+	}
 }
